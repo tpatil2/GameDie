85400111a3b2f7f3b101b58fc5f315a252c36708
kbuffardi <kbuffardi@csuchico.edu>
2016-01-28 22:51:00 -0800

    Creates initial interface for GameDie

---
+++ GameDie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+//class constructor that seeds the random number generator
+GameDie::GameDie()
+{
+}
+
+//generate a random number between 1-6 (inclusive) and display it
+void GameDie::roll()
+{
+}
---
+++ GameDie.h
@@ -0,0 +1,12 @@
+#ifndef GAMEDIE_H
+#define GAMEDIE_H
+
+class GameDie
+{
+  public:
+   GameDie();
+   void roll();
+};
+
+#include "GameDie.cpp"
+#endif
